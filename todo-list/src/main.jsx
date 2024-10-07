c395035555ae8337dba449930435b57a4c9c3ce0
hemant.kamliya <hemantkamliya87@gmail.com>
2024-10-07 23:12:22 +0530

    Created a new project

--- todo-list/src/main.jsx
+++ todo-list/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-const anotherElement=React.createElement('a',
-  {href:'//www.youtube.com',target :'_blank'},
- " Visit google"
-)
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-     anotherElement
-  
+    <App />
+ 
 )
