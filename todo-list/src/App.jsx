ec9fd75c1b72f5112c6de95695d6b1357d2d5a86
hemant.kamliya <hemantkamliya87@gmail.com>
2024-10-23 22:55:05 +0530

    Added some css

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <div className='bg-gradient-to-r from-purple-600 to-blue-400 min-h-screen px-10 py-20 flex flex-col items-center'>
         <div className='mb-8 text-white text-6xl font-extrabold tracking-wider'>
-          Todo List
+       to-do list
         </div>
         <div className='w-full max-w-2xl bg-white shadow-xl rounded-3xl p-10'>
           <InputComponent />
